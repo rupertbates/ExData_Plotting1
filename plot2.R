3462eea932e98314e238eff20961f308490e9caa
rbates <rupert.bates@gmail.com>
2014-08-04 22:17:22 +0100

    Plot 2

---
+++ plot2.R
@@ -0,0 +1,20 @@
+# Course project 1, plot 2
+
+# Read the header row 
+headers <- read.table("./household_power_consumption.txt", sep=";", stringsAsFactors=FALSE, nrows=1)
+headers <- as.character(headers[1,])
+
+# Read in rows for the dates 2007-02-01 and 2007-02-02
+twoDays = (60 * 24 * 2) + 1
+data <- read.table("./household_power_consumption.txt", sep=";", col.names=headers, skip=66637, nrows=twoDays)
+
+# Convert the dates to POSIXlt
+data$DateParsed <- strptime(data$Date, format="%d/%m/%Y")
+data$WeekDay <- weekdays(data$DateParsed)
+
+plot(data$Global_active_power, type="l", xaxt=FALSE)
+axis(side=1, at=c(0,1440, 2880), labels=c("Thursday", "Friday", "Saturday"))
+#hist(data$Global_active_power, col = "Red", main = "Global Active Power", xlab="Global Active Power (kilowatts)")
+
+dev.copy(png, file = "plot2.png") 
+dev.off()
