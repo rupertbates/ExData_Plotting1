5cb138abdf4af77fcbb85d0c3a9336223449771f
rupertbates <rupert.bates@guardian.co.uk>
2014-08-04 17:58:16 +0100

    First plot

--- plot1.R
+++ plot1.R
@@ -1,4 +1,9 @@
 # Course project 1, plot 1
-# 2007-02-01 and 2007-02-02
-data <- read.table("./household_power_consumption.txt", sep=";", skip=66636, nrows=4000)
+
+# Read the header row 
+headers <- read.table("./household_power_consumption.txt", sep=";", stringsAsFactors=FALSE, nrows=1)
+headers <- as.character(headers[1,])
+
+# Read in rows for the dates 2007-02-01 and 2007-02-02
+data <- read.table("./household_power_consumption.txt", sep=";", col.names=headers, skip=66637, nrows=2880)
 
