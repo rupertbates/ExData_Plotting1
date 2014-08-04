aaa1c32f3ece6752943f92e702f2de9a70ac459a
rbates <rupert.bates@gmail.com>
2014-08-04 19:58:22 +0100

    Create plot1 & save to a file

--- plot1.R
+++ plot1.R
@@ -5,5 +5,13 @@ headers <- read.table("./household_power_consumption.txt", sep=";", stringsAsFac
 headers <- as.character(headers[1,])
 
 # Read in rows for the dates 2007-02-01 and 2007-02-02
-data <- read.table("./household_power_consumption.txt", sep=";", col.names=headers, skip=66637, nrows=2880)
+twoDays = 60 * 24 * 2
+data <- read.table("./household_power_consumption.txt", sep=";", col.names=headers, skip=66637, nrows=twoDays)
 
+# Convert the dates to POSIXlt
+data$DateParsed <- strptime(data$Date, format="%d/%m/%Y")
+
+hist(data$Global_active_power, col = "Red", main = "Global Active Power", xlab="Global Active Power (kilowatts)")
+
+dev.copy(png, file = "plot1.png") 
+dev.off()
